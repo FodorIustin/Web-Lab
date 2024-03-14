53c0d039736c06909fb65c5edec6c1ad6c1ccd92
FodorIustin <126153569+FodorIustin@users.noreply.github.com>
2024-03-14 14:02:18 +0200

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
-<script setup>
-import ToDoView from './components/ToDoView.vue';
-</script>
-
 <template>
-  <header>
-    <h1>My first TODO list in Vue.js</h1>
-  </header>
-  <main>
-    <ToDoView></ToDoView>
-  </main>
+  <div class="container mx-auto">
+    <Card v-for="product in products" :key="product.name" :product="product" />
+  </div>
 </template>
 
+<script setup>
+import { ref } from 'vue';
+import Card from './components/Card.vue';
+
+const products = ref([
+  {
+    name: "Montes Scelerisque",
+    stars: "4.9",
+    reviews: "807",
+    sale: "65",
+    price: "332",
+    image: "https://via.placeholder.com/150",
+  },
+ 
+]);
+</script>
+
 <style scoped>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App.vue'
 
-import { createPinia } from 'pinia'
-const pinia = createPinia();
-
-createApp(App).use(pinia).mount('#app')
+createApp(App).mount('#app')
